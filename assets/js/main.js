b1751197ba5eaf2c9abdcfe5b52f61d996ce94ec
partcollector <info@partcollector.org>
2025-12-13 13:22:24 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -291,12 +291,7 @@
 
 				var $this = $(this);
 
-				// Close.
-					$('<div class="close">Close</div>')
-						.appendTo($this)
-						.on('click', function() {
-							location.hash = '';
-						});
+				
 
 				// Prevent clicks from inside article from bubbling.
 					$this.on('click', function(event) {
